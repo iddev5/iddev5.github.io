b6e4d7dc708584e82ff718e4c2dcbedae8845949
iddev5 <ayushtechportal@gmail.com>
2020-11-20 13:13:06 +0530

    Moved script to a separate file

---
+++ script.js
@@ -0,0 +1,24 @@
+let theme = "light";
+
+let light = {};
+light.background = "#ffffff";
+light.color = "#000000";
+light.anylink = "#08088A";
+light.themebutton = "Light Mode";			
+
+let dark = {};
+dark.background = "#000000";
+dark.color = "#ffffff";
+dark.anylink = "#ff0000";
+dark.themebutton = "Dark Mode";
+
+function SwitchTheme() {
+	let current = (theme === "light") ? dark : light;
+
+	document.body.style.background = current.background;
+	document.body.style.color = current.color;
+	document.getElementById("anylink").style.color = current.anylink;
+	document.getElementById("themebutton").text = current.themebutton;
+
+	theme = (theme === "light") ? "dark" : "light"; 
+}
