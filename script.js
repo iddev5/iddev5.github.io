834a875492659014ad4214e1fc3fa482d15bc2f8
Ayush <54106488+iddev5@users.noreply.github.com>
2021-01-27 21:11:40 +0530

    Made the webpage much more responsive by changing width at short screen sizes (#1)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let light = {};
 light.background = "#ffffff";
 light.color = "#000000";
 light.anylink = "#08088A";
-light.themebutton = "Light Mode";			
+light.themebutton = "Light Mode";	
 
 let dark = {};
 dark.background = "#000000";
@@ -12,16 +12,33 @@ dark.color = "#ffffff";
 dark.anylink = "#ff0000";
 dark.themebutton = "Dark Mode";
 
+function isMobile() {
+	return (window.innerWidth <= 800) && (window.innerHeight <= 600);
+}
+
 function SwitchTheme() {
 	let current = (theme === "light") ? dark : light;
 
 	document.body.style.background = current.background;
 	document.body.style.color = current.color;
+	
 	document.getElementById("anylink").style.color = current.anylink;
 	document.getElementById("themebutton").text = current.themebutton;
 
 	theme = (theme === "light") ? "dark" : "light"; 
 }
 
-// Switch to dark theme as soon as page loads
-SwitchTheme();
+/* Dynamically set width */
+window.addEventListener("resize", () => {
+	if(isMobile()) {
+		document.body.style.width = "85%";
+	}
+	else {
+		document.body.style.width = "60%";
+	}
+});
+
+/* Switch to dark theme as soon as page loads */
+window.addEventListener("load", () => { 
+	SwitchTheme();
+});
